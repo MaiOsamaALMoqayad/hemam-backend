f810f75cb12a6e7c42a29c46e66d1c4fa5d6d53e
MaiOsamaALMoqayad <maiosama338@gmail.com>
2025-11-29 12:16:00 +0200

    Add CORS configuration file

---
+++ config/cors.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    */
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'https://hemam-rouge.vercel.app',
+        'http://localhost:3000',
+        env('FRONTEND_URL'),
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+
+];
